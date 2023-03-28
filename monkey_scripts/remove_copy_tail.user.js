6904116617987eae2f060c5c3ac75cdf116f7a5b
y410p <y410p@linxu.com>
2023-03-29 00:25:58 +0800

    Version 2.0

--- monkey_scripts/remove_copy_tail.user.js
+++ monkey_scripts/remove_copy_tail.user.js
@@ -5,6 +5,8 @@
 // @grant       none
 // @version     2.0
 // @author      -
+// @downloadURL https://github.com/bytedynamic/mirror/raw/main/monkey_scripts/remove_copy_tail.user.js
+// @updateURL   https://github.com/bytedynamic/mirror/raw/main/monkey_scripts/remove_copy_tail.user.js
 // @description 2023/3/29 0:17
 // ==/UserScript==
 [...document.querySelectorAll('*')].forEach(item=>{
