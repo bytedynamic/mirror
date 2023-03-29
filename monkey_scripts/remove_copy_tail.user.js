d3e3efddeb34c5e92a2aaf1b9ac0c53eb772eac3
bytedynamic <54274219+bytedynamic@users.noreply.github.com>
2023-03-29 12:46:44 +0800

    修复匹配域名，添加对zhihu的支持

--- monkey_scripts/remove_copy_tail.user.js
+++ monkey_scripts/remove_copy_tail.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name        去掉小尾巴
 // @namespace   Violentmonkey Scripts
-// @match       https://wallstreetcn.com/live/global
+// @match       https://wallstreetcn.com/*
+// @match       https://*.zhihu.com/*
 // @grant       none
-// @version     2.0
+// @version     2.1
 // @author      LX
 // @downloadURL https://github.com/bytedynamic/mirror/raw/main/monkey_scripts/remove_copy_tail.user.js
 // @updateURL   https://github.com/bytedynamic/mirror/raw/main/monkey_scripts/remove_copy_tail.user.js
-// @description 2023/3/29 0:17
+// @description 2023/3/29 12:45
 // ==/UserScript==
 [...document.querySelectorAll('*')].forEach(item=>{
     item.oncopy = function(e) {
