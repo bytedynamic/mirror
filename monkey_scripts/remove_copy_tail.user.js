1d5fbef1fae21e9888bed6f693e1e278b68fdc84
bytedynamic <54274219+bytedynamic@users.noreply.github.com>
2023-03-30 14:55:41 +0800

    extend supported websites

--- monkey_scripts/remove_copy_tail.user.js
+++ monkey_scripts/remove_copy_tail.user.js
@@ -1,16 +1,15 @@
 // ==UserScript==
 // @name        去掉小尾巴
 // @namespace   Violentmonkey Scripts
-// @match       https://wallstreetcn.com/*
-// @match       https://*.zhihu.com/*
+// @match       *://*/*
 // @grant       none
-// @version     2.2
+// @version     2.3
 // @author      LX
 // @supportURL  https://github.com/bytedynamic/mirror/tree/main/monkey_scripts
 // @homepageURL https://github.com/bytedynamic/mirror/tree/main/monkey_scripts
 // @downloadURL https://ghproxy.net/https://raw.githubusercontent.com/bytedynamic/mirror/main/monkey_scripts/remove_copy_tail.user.js
 // @updateURL   https://ghproxy.net/https://raw.githubusercontent.com/bytedynamic/mirror/main/monkey_scripts/remove_copy_tail.user.js
-// @description 2023/3/29 13:55
+// @description 2023/3/30 15:00
 // ==/UserScript==
 [...document.querySelectorAll('*')].forEach(item=>{
     item.oncopy = function(e) {
