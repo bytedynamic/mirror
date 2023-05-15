9e112072f283d04795f36fcf6e5d885c1ba7c2b3
y410p <y410p@linxu.com>
2023-05-16 01:11:16 +0800

    更新channel_v3.json

--- update_channel_v3.bat
+++ update_channel_v3.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d %~dp0
 del /Q channel_v3.json
-curl -o channel_v3.json https://packagecontrol.io/channel_v3.json
+aria2c -o channel_v3.json https://packagecontrol.io/channel_v3.json
 pause
 
